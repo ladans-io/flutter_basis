d4a2f8f647af3854f629760288c10cf4ee258f3f
ladans.io <ladino.anselmo@gmail.com>
2023-12-26 15:09:30 +0200

    build: v1.0.5+10

--- lib/src/widgets/loading/f_loading_card.dart
+++ lib/src/widgets/loading/f_loading_card.dart
@@ -1,92 +1,104 @@
-import 'dart:ui' as ui;
-
 import 'package:flutter/material.dart';
 import 'package:flutter_basis/flutter_basis.dart';
 
-class FLoadingCard extends StatelessWidget with ResponsiveSizes {
+class LoadingCard extends StatelessWidget with ResponsiveSizes {
   final bool onWillPop;
   final String? loadingPlaceholder;
-  final Color? color;
   final VoidCallback? onCancel;
 
-  const FLoadingCard({
+  const LoadingCard({
     Key? key, 
     this.onWillPop = false, 
     this.loadingPlaceholder,
-    this.color,
     this.onCancel,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.bottomCenter,
-      children: [
-        WillPopScope(
-          onWillPop: () async => onWillPop,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+    return WillPopScope(
+      onWillPop: () async => onWillPop,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Stack(
+            clipBehavior: Clip.none,
+            alignment: Alignment.topRight,
             children: [
-              Card(
-                margin: EdgeInsets.zero,
-                elevation: 0,
-                color: Colors.transparent,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(kRespValue(context, 8, 2)),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: dp20(context) * 2),
-                  child: BackdropFilter(
-                    filter: ui.ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: FLoading(color: color ?? Theme.of(context).colorScheme.secondary),
-                        ),
-                        SizedBox(height: dp10(context)),
-                        if (loadingPlaceholder != null) FText(
-                          loadingPlaceholder!,
-                          fontSize: dp16(context),
-                          alignCenter: true,
-                          bold: true,
-                          color: Colors.black54,
-                        ),
-                      ],
+              SizedBox(
+                width: screenWidth(context) * .5,
+                child: Card(
+                  margin: EdgeInsets.zero,
+                  elevation: 0,
+                  color: Color(0xFF303030).withOpacity(.9),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      dp4(context),
                     ),
+                  ),
+                  child: _content(context),
+                ),
+              ),
+
+              if (onCancel != null) Positioned(
+                child: SizedBox(
+                  height: dp30(context),
+                  width: dp30(context),
+                  child: FloatingActionButton(
+                    tooltip: 'Cancelar requisição',
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(dp4(context)),
+                      ),
+                    ),
+                    elevation: 1,
+                    backgroundColor: Color(0xFFFF5252),
+                    onPressed: onCancel ?? Navigate.to.pop,
+                    child: const Icon(Icons.close, color: Colors.white),
                   ),
                 ),
               ),
             ],
           ),
-        ),
-        if (onCancel != null) Positioned(
-          bottom: dp25(context) * 2,
-          child: FloatingActionButton(
-            tooltip: 'Cancelar requisição',
-            elevation: 1,
-            backgroundColor: Colors.red,
-            onPressed: onCancel ?? Navigate.to.pop,
-            child: const Icon(Icons.close, color: Colors.white),
+        ],
+      ),
+    );
+  }
+
+  Widget _content(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        vertical: dp15(context),
+        horizontal: dp10(context),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          FLoading(color: Colors.white, size: dp30(context)),
+
+          SizedBox(height: dp10(context)),
+
+          if (loadingPlaceholder != null) FText(
+            loadingPlaceholder!,
+            fontSize: dp14(context),
+            color: Colors.white,
+            alignCenter: true,
+            light: true,
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 
   static show({
     String? loadingPlaceholder, 
-    Color? color, 
     VoidCallback? onCancel,
   }) {
     showDialog(
       barrierDismissible: false,
       context: Navigate.navigatorKey.currentState!.overlay!.context, 
       barrierColor: Colors.transparent,
-      builder: (context) => FLoadingCard(
-        loadingPlaceholder: loadingPlaceholder, 
-        color: color,
+      builder: (context) => LoadingCard(
+        loadingPlaceholder: loadingPlaceholder,
         onCancel: onCancel,
       ),
     );
