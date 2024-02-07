518f1d0d8f3f10bae70ed899bea4db17da1a624c
ladans.io <ladino.anselmo@gmail.com>
2024-02-08 00:41:31 +0200

    refactor: basis client http add name url base param

--- lib/src/application/remote/basis_http_client.dart
+++ lib/src/application/remote/basis_http_client.dart
@@ -7,7 +7,7 @@ import 'package:http/http.dart' as http;
 class BasisHttpClient extends http.BaseClient with AuthHeaders {
   late String urlBase;
 
-  BasisHttpClient(String? urlBase) {
+  BasisHttpClient({String? urlBase}) {
     this.urlBase = urlBase ?? String.fromEnvironment('URL_BASE');
   }
 
