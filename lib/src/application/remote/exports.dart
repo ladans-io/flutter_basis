e95663c9a595f8f4eedc27e00840476f5053e7f6
ladans.io <ladino.anselmo@gmail.com>
2024-02-10 23:13:06 +0200

    refactor: change class name

--- lib/src/application/remote/exports.dart
+++ lib/src/application/remote/exports.dart
@@ -1,5 +1,5 @@
 export 'client_request_handler.dart';
 export 'basis_http_client.dart';
-export 'remote_failure_or_success.dart';
+export 'client_either_response_handler.dart';
 export 'error_handler.dart';
 export 'client_response.dart';
