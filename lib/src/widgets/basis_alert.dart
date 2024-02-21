e5a0dee2e80911abce28d0808832d9224733d510
ladans.io <ladino.anselmo@gmail.com>
2024-02-21 16:46:23 +0200

    refactor: basis alert

--- lib/src/widgets/basis_alert.dart
+++ lib/src/widgets/basis_alert.dart
@@ -6,26 +6,40 @@ const Color _successColor = Color(0xFF4CAF50);
 const Color _errorColor = Color(0xFFFF5252);
 final Color _infoColor = Colors.amber.shade800;
 
+void closeAlert() {
+  GlobalKeys.scaffoldMessengerKey.currentState?.clearSnackBars();
+}
+
 class BasisAlert {
   static show({
     required String message,
     required AlertState state,
     String? prefixIcon,
     Color? backgroundColor,
-    EdgeInsetsGeometry? margin,
+    EdgeInsetsGeometry? margin, padding,
+    AlertBehavior? behavior,
+    BorderRadiusGeometry? radius,
+    Duration duration = const Duration(seconds: 3),
+    DismissDirection? dismissDirection,
   }) {
     return GlobalKeys.scaffoldMessengerKey.currentState?.showSnackBar(
       SnackBar(
         elevation: 0,
-        backgroundColor: Colors.transparent,
-        margin: margin ?? const EdgeInsets.symmetric(horizontal: 40),
+        backgroundColor: backgroundColor ?? _snackBarBgColor,
+        dismissDirection: dismissDirection ?? DismissDirection.down,
+        duration: duration,
+        shape: RoundedRectangleBorder(borderRadius: radius ?? BorderRadius.circular(4)),
+        padding: padding,
+        margin: behavior?.value == 1
+          ? margin ?? const EdgeInsets.symmetric(horizontal: 20, vertical: 10)
+          : null,
         content: AlertContent(
-          message: message, 
-          state: state, 
+          message: message,
+          state: state,
           prefixIcon: prefixIcon, 
           backgroundColor: backgroundColor,
         ),
-        behavior: SnackBarBehavior.floating,
+        behavior: behavior?.getBehavior(),
       ),
     );
   }
@@ -47,53 +61,60 @@ class AlertContent extends StatelessWidget with ResponsiveSizes {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(dp20(context)),
-      child: Container(
-        width: screenWidth(context),
-        padding: EdgeInsets.all(dp15(context)),
-        constraints: BoxConstraints(maxHeight: screenHeight(context) * .5),
-        color: backgroundColor ?? _snackBarBgColor,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            if (prefixIcon != null) ...[
-              Container(
-                width: dp22(context),
-                height: dp22(context),
-                decoration: BoxDecoration(
-                  image: DecorationImage(image: AssetImage(prefixIcon!)),
-                ),
-              ),
-              SizedBox(width: dp10(context)),
-            ],
-            Expanded(
-              child: BasisText(
-                message,
-                color: Colors.white,
-                light: true,
-                overflowFade: true,
-              ),
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        if (prefixIcon != null) ...[
+          Container(
+            width: dp22(context),
+            height: dp22(context),
+            decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage(prefixIcon!)),
             ),
-            Icon(
-              switch (state) {
-                AlertState.success => Icons.check_circle,
-                AlertState.info => Icons.info_rounded,
-                AlertState.error => Icons.error_rounded,
-              },
-              color: switch (state) {
-                AlertState.success => _successColor,
-                AlertState.info => _infoColor,
-                AlertState.error => _errorColor,
-              },
-              size: dp28(context),
-            ),
-          ],
+          ),
+          SizedBox(width: dp10(context)),
+        ],
+        Expanded(
+          child: BasisText(
+            message,
+            color: Colors.white,
+            light: true,
+            overflowFade: true,
+          ),
         ),
-      ),
+        Icon(
+          switch (state) {
+            AlertState.success => Icons.check_circle,
+            AlertState.info => Icons.info_rounded,
+            AlertState.error => Icons.error_rounded,
+          },
+          color: switch (state) {
+            AlertState.success => _successColor,
+            AlertState.info => _infoColor,
+            AlertState.error => _errorColor,
+          },
+          size: dp28(context),
+        ),
+      ],
     );
   }
 }
 
 enum AlertState { error, info, success }
+
+enum AlertBehavior {
+  floating(1),
+  fixed(2);
+
+  const AlertBehavior(this.value);
+  final int value;
+
+  SnackBarBehavior getBehavior() {
+    if (value == 1) {
+      return SnackBarBehavior.floating;
+    } else {
+      return SnackBarBehavior.fixed;
+    }
+  }
+}
 
