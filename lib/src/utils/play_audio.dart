5820283e46e041983a3e8da952953fb6c9b72f18
ladans.io <ladino.anselmo@gmail.com>
2024-03-19 23:40:32 +0200

    refactor: palyer

--- lib/src/utils/play_audio.dart
+++ lib/src/utils/play_audio.dart
@@ -2,6 +2,8 @@ import 'package:audioplayers/audioplayers.dart';
 
 Future<void> playAudio(String sound) async {
   final player = AudioPlayer();
-  await player.play(AssetSource(sound));
-  await player.dispose();
+  
+  await player.setVolume(1);
+  await player.setSourceAsset(sound);
+  await player.resume();
 }
