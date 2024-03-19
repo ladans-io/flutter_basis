66765a376c47aac614421dfaad77f2680cae3f20
ladans.io <ladino.anselmo@gmail.com>
2024-03-20 00:03:35 +0200

    feat: increase volume

--- lib/src/utils/play_audio.dart
+++ lib/src/utils/play_audio.dart
@@ -3,7 +3,7 @@ import 'package:audioplayers/audioplayers.dart';
 Future<void> playAudio(String sound) async {
   final player = AudioPlayer();
 
-  await player.setVolume(3);
+  await player.setVolume(1);
   await player.setSourceAsset(sound);
   await player.resume();
 }
