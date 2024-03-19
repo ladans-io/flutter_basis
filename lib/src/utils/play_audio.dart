3818e271920bde1add2554d2ca0a9bccc968a21c
ladans.io <ladino.anselmo@gmail.com>
2024-03-19 23:56:01 +0200

    fix: player increase volume

--- lib/src/utils/play_audio.dart
+++ lib/src/utils/play_audio.dart
@@ -2,8 +2,8 @@ import 'package:audioplayers/audioplayers.dart';
 
 Future<void> playAudio(String sound) async {
   final player = AudioPlayer();
-  
-  await player.setVolume(1);
+
+  await player.setVolume(3);
   await player.setSourceAsset(sound);
   await player.resume();
 }
